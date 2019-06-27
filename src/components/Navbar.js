5d32b3cf654487f122dafe75e8beec9fb458bb35
TaphieM <taffymayen@gmail.com>
2019-06-27 11:32:54 -0500

    mejoras en colores y diseño en Navbar solo falta cambiar la flecha de AllPins

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,27 +2,28 @@ import React from 'react';
 import './Navbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPinterest } from '@fortawesome/free-brands-svg-icons';
-import {faBell, faCommentDots, faEllipsisH, faUserCircle, faSearch} from '@fortawesome/free-solid-svg-icons'
+import {faBell, faCommentDots, faEllipsisH, faUserCircle, faSearch} from '@fortawesome/free-solid-svg-icons';
 
 class Navbar extends React.Component{
  render(){
      return(
          <header className="navbar">
-            <div><FontAwesomeIcon className="pinterest" icon={faPinterest} /></div>
-            <div className="bar">
+            <button><FontAwesomeIcon className="pinterest" icon={faPinterest} /></button>
+            {/* <div className="bar"> */}
              <FontAwesomeIcon className="loop" icon={faSearch} />
              <input className="search" type="text" placeholder="Search"/>
-             <div className="custom-select">
-                <select>
-              <option>All pines</option>
+                <select className="custom-select">
+              <option>All Pins</option>
                 </select>
-             </div>
-             <FontAwesomeIcon className="icons" icon={faUserCircle} />
-             <span>Stephanie</span>
-             <FontAwesomeIcon className="icons" icon={faCommentDots} />
-             <FontAwesomeIcon className="icons" icon={faBell} />
-             <FontAwesomeIcon className="icons" icon={faEllipsisH} /> 
-             </div>  
+            {/* </div>
+            <div> */}
+             <button><strong>Home</strong></button>
+             <button><strong>Following</strong></button>
+             <button><FontAwesomeIcon className="icons" icon={faUserCircle} /> <strong>Stephanie</strong></button>
+             <button><FontAwesomeIcon className="icons" icon={faCommentDots} /></button>
+             <button><FontAwesomeIcon className="icons" icon={faBell} /></button>
+             <button><FontAwesomeIcon className="icons" icon={faEllipsisH} /></button>  
+             {/* </div> */}
          </header>
      )
  }
