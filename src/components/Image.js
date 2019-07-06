560ec595d256cda3781081af9f24c3eff12702ac
TaphieM <taffymayen@gmail.com>
2019-07-06 15:03:02 -0500

    mejora de comentarios

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,8 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisH, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 
-
-
 //onClick={()=>props.modal(props.url)} va en el img para ver el modal
  const Image = ({item}) => {
 
@@ -13,7 +11,7 @@ import { faEllipsisH, faChevronDown } from '@fortawesome/free-solid-svg-icons';
   const {alt_description, urls, tags} = item;
     return (
         <div className="card">
-        {/*    */}
+        {/* <div><button className="tag">{tags[0].title}<FontAwesomeIcon className="narrow" icon={faChevronDown} /></button><button className="save">Save</button></div> */}
         <div className="cardImage">
         <img className="imgCard" src={urls.thumb} alt={alt_description}></img>
         <button className="more"><FontAwesomeIcon  icon={faEllipsisH} /></button>
